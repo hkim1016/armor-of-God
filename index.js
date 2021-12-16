b1c1097f89c2e6c5d3746916d40b604f5b166e96
hkim1016 <hankim0827@gmail.com>
2021-12-15 23:05:16 -0800

    Set public directory as the holder for static files

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(process.cwd() + '/public'));
 app.set('view engine', 'ejs');
 
 app.use('/', require('./routes'))
