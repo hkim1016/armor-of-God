ba8da17b742465f31b3a90012d29ae5edc02eca9
hkim1016 <hankim0827@gmail.com>
2021-12-16 01:14:38 -0800

    added navbar to every page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', (req, res) => {
     res.render('../views/index');
 });
 
+router.get('/about', (req, res) => {
+    res.render('../views/about');
+});
+
+router.get('/contact', (req, res) => {
+    res.render('../views/contact');
+});
+
 module.exports = router;
