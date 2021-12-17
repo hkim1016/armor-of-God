7386b8cf757981b9739f2e0c7f21de889a7755f6
hkim1016 <hankim0827@gmail.com>
2021-12-16 20:43:02 -0800

    Update login/signup page

--- public/scripts/login-signup.js
+++ public/scripts/login-signup.js
@@ -1,20 +1,30 @@
 $(".signup-form").hide();
-$(".signup").css("background", "#fff");
+$(".login").css("background", "#F5F3F3");
+
+$(".signup").hover(() => {
+    $("this").css("background-color", "#F5F3F3");
+});
+
+$(".login").hover(() => {
+    $("this").css("background-color", "#F5F3F3");
+});
 
 $(".login").click(() => {
     $(".signup-form").hide();
     $(".login-form").show();
-    $(".signup").css("background", "#fff");
+    $(".signup").css("background", "none");
     $(".login").css("background", "#F5F3F3");
-    $(".signup").css("border-radius", "0px");
-    $(".login").css("border-radius", "30px");
+    $(".signup-form input").val("");
+    // $(".signup").css("border-radius", "0px");
+    // $(".login").css("border-radius", "30px");
 });
 
 $(".signup").click(() => {
     $(".signup-form").show();
     $(".login-form").hide();
-    $(".login").css("background", "#fff");
+    $(".login").css("background", "none");
     $(".signup").css("background", "#F5F3F3");
-    $(".signup").css("border-radius", "30px");
-    $(".login").css("border-radius", "0px");
+    $(".login-form input").val("");
+    // $(".signup").css("border-radius", "30px");
+    // $(".login").css("border-radius", "0px");
 });
