a4af8296b0f148c2a07ffe647d5a108902a48f14
hkim1016 <hankim0827@gmail.com>
2021-12-23 19:14:27 -0800

    made navbar responsive to screen size and moved some css

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,3 +1,17 @@
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+    overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+    overlay.classList.remove("overlay--active");
+});
+
+
 document.querySelector('.logo').addEventListener('click', () => {
     window.location = "/";
 });
